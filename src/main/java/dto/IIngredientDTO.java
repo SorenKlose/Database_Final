bdbce66c047987804dbd2639206a25e1911c2cf2
Søren Klose <klose29@live.dk>
2019-05-07 10:19:09 +0200

    Filled out the ingredientDTO interface

--- src/main/java/dto/IIngredientDTO.java
+++ src/main/java/dto/IIngredientDTO.java
@@ -1,4 +1,17 @@
 package dto;
 
 public interface IIngredientDTO {
+	int getIngredientId();
+
+	void setIngredientId(int userId);
+
+	String getIngredientName();
+
+	void setIngredientName(String userName);
+
+	String getMargin();
+
+	void setMargin();
+
+	boolean active();
 }
