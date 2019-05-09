b372d1ae04befb8b5d45570442c5b58e42c7e252
Lukas Dahl <s160068@student.dtu.dk>
2019-05-09 10:57:44 +0200

    Product extends exception

--- src/main/java/dal/IProductDAO.java
+++ src/main/java/dal/IProductDAO.java
@@ -4,7 +4,7 @@ import dto.IProductDTO;
 
 import java.util.List;
 
-public interface IProductDAO {
+public interface IProductDAO extends IDALException{
 
     void createProduct(IProductDTO product) throws DALException;
 
@@ -13,18 +13,5 @@ public interface IProductDAO {
     List<IProductDTO> getProductList() throws DALException;
 
     void updateProduct(IProductDTO product) throws DALException;
-
-    public class DALException extends Exception {
-        private static final long serialVersionUID = 7355418246336739229L;
-
-        public DALException(String msg, Throwable e) {
-            super(msg,e);
-        }
-
-        public DALException(String msg) {
-            super(msg);
-        }
-
-    }
 
 }
