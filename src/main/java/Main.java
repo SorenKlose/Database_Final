02b081c32dcfc1f89b486c97e26e36f89670f9f8
Lukas Dahl <s160068@student.dtu.dk>
2019-05-09 11:26:20 +0200

    DAO objects made

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
+import dal.*;
+import dto.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        IUserDAO userDAO = new UserDAO();
+        IMaterialDAO materialDAO = new MaterialDAO();
+        IProductDAO productDAO = new ProductDAO();
+        IRecipeDAO recipeDAO = new RecipeDAO();
+        IIngredientDAO ingredientDAO = new IngredientDAO();
+
+
+
+
+
     }
 }
